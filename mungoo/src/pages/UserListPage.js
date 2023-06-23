c12fa7ecf05d55fa6e4566f037a02a07bcc31a68
takyeoung <103091272+takyeoung@users.noreply.github.com>
2023-06-23 17:03:15 +0900

    [update] 유저삭제

--- mungoo/src/pages/UserListPage.js
+++ mungoo/src/pages/UserListPage.js
@@ -26,7 +26,7 @@ const UserListPage = () => {
         console.error(error);
       }
     };
-
+  
     fetchUsers();
   }, [searchQuery]);
 
@@ -38,17 +38,60 @@ const UserListPage = () => {
     );
   });
 
+  const updateUserGrade = async (uno, grade) => {
+    try {
+      const response = await axios.put(
+        `${API_URL}/user/update/${uno}`,
+        { grade },
+        {
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+          },
+        }
+      );
+
+      // 등급 업데이트에 성공하면 서버로부터 업데이트된 사용자 정보를 받아와서 업데이트합니다.
+      if (response.status === 200) {
+        const updatedUser = response.data;
+        setUsers((prevUsers) =>
+          prevUsers.map((user) =>
+            user.uno === updatedUser.uno ? updatedUser : user
+          )
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteUser = async (uno) => {
+    try {
+      console.log(uno);
+      const response = await axios.delete(`${API_URL}/user/delete/${uno}`, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        },
+      });
+      if (response.status === 204) {
+        const updatedUsers = users.filter((user) => user.uno !== uno);
+        setUsers(updatedUsers);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className='UserList'>
-      <h1 className='UserList-h1'>유저 리스트 페이지</h1>
+    <div className="UserList">
+      <h1 className="UserList-h1">유저 리스트 페이지</h1>
       <input
-        className='UserList-Input'
+        className="UserList-Input"
         type="text"
         placeholder="유저 검색"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
-      <table className='UserList-table'>
+      <table className="UserList-table">
         <thead>
           <tr>
             <th>아이디</th>
@@ -63,10 +106,23 @@ const UserListPage = () => {
             <tr key={user.uno}>
               <td>{user.uid}</td>
               <td>{user.uname}</td>
-              <td></td>
-              <td></td>
+              <td>{new Date(user.regDate).toLocaleDateString()}</td>
               <td>
-                <span className='DeleteIcon'>
+                <select
+                  value={user.grade}
+                  onChange={(e) => updateUserGrade(user.uno, e.target.value)}
+                >
+                  <option value="ADMIN">ADMIN</option>
+                  <option value="BEN">BEN</option>
+                  <option value="DROP">DROP</option>
+                  <option value="USER">USER</option>
+                </select>
+              </td>
+              <td>
+                <span
+                  className="DeleteIcon"
+                  onClick={() => deleteUser(user.uno)}
+                >
                   <TiDelete style={{ fontSize: '24px' }} />
                 </span>
               </td>
